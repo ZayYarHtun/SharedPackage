170ce6987c5b9193e97d843ac2f905bf29f9c249
Zayar <zayariosdev@icloud.com>
2023-02-03 01:12:38 +0700

    Renamed it to SharedFrameworks and added needed dependencies

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "SharePackage",
+    name: "SharedFrameworks",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SharePackage",
-            targets: ["SharePackage", "MobileVLCKit", "Realm"]),
+            name: "SharedFrameworks",
+            targets: ["SharedFrameworks", "MobileVLCKit", "Realm"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SharePackage",
+            name: "SharedFrameworks",
             dependencies: []),
         .binaryTarget(
                     name: "MobileVLCKit",
@@ -34,3 +34,4 @@ let package = Package(
        
     ]
 )
+
--- Sources/SharedFrameworks/SharedFrameworks.swift
+++ Sources/SharedFrameworks/SharedFrameworks.swift
@@ -1,4 +1,4 @@
-public struct SharePackage {
+public struct SharedFrameworks {
     public private(set) var text = "Hello, World!"
 
     public init() {
